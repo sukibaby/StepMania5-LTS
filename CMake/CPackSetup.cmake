47db975cf03fb91cc6856657744da14a53d7097a
sukibaby <163092272+sukibaby@users.noreply.github.com>
2025-07-12 02:01:42 -0700

    hope

--- CMake/CPackSetup.cmake
+++ CMake/CPackSetup.cmake
@@ -9,7 +9,7 @@ if(WITH_FULL_RELEASE)
 else()
   set(CPACK_PACKAGE_VERSION "${SM_VERSION_GIT}")
 endif()
-set(CPACK_PACKAGE_EXECUTABLES "${SM_EXE_NAME}" "SM5-LTS")
+set(CPACK_PACKAGE_EXECUTABLES "${SM_EXE_NAME}" "StepMania 5 LTS")
 set(CPACK_RESOURCE_FILE_README "${SM_ROOT_DIR}/README.md")
 set(CPACK_RESOURCE_FILE_LICENSE "${SM_CMAKE_DIR}/license_install.txt")
 
@@ -21,8 +21,8 @@ if(WIN32)
   # major.minor.patch" is lost. This is currently done to maintain backwards
   # compatibility. However, removing these two will allow for multiple versions
   # of StepMania to be installed relatively cleanly.
-  set(CPACK_PACKAGE_INSTALL_DIRECTORY "SM5-LTS")
-  set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "SM5-LTS")
+  set(CPACK_PACKAGE_INSTALL_DIRECTORY "StepMania 5 LTS")
+  set(CPACK_PACKAGE_INSTALL_REGISTRY_KEY "StepMania 5 LTS")
   set(CPACK_NSIS_EXECUTABLES_DIRECTORY "Program")
   set(CPACK_NSIS_INSTALL_ROOT "C:\\\\Games")
 
